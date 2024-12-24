ef83c7114aab5ca1a0f57187c91d0f7d1c39e877
David Fdez Alonso <fdezd053@gmail.com>
2024-12-24 14:41:27 -0500

    Dropdowns in Kiosko

--- src/lib/client/components/kiosko/KioskoCategory.svelte
+++ src/lib/client/components/kiosko/KioskoCategory.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
     import type { KioskoCategory } from '$lib/types/kiosko';
     import KioskoItemCard from './KioskoItemCard.svelte';
-       
+  import '../menu/Menu_Kiosko.css'   
+
     export let category: KioskoCategory;
   </script>
   
   <section>
-    <h2 class="text-2xl font-bold mb-6">{category.name}</h2>
-    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {#each category.items as item}
-        <KioskoItemCard {item} />
-      {/each}
-    </div>
+    <details class="border rounded-lg overflow-hidden transition-all duration-300 ease-in-out">
+      <summary class="text-2xl font-bold mb-0 cursor-pointer bg-gray-100 p-4 hover:bg-gray-200">
+        <span>{category.name}</span>
+        <span class="view-more">Ver Más</span>
+      </summary>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center p-4 bg-white">
+        {#each category.items as item}
+          <KioskoItemCard {item} />
+        {/each}
+      </div>
+    </details>
   </section>
--- src/lib/client/components/ordenar/OrdenarPedido.svelte
+++ src/lib/client/components/ordenar/OrdenarPedido.svelte
@@ -3,6 +3,7 @@
   import { onMount } from 'svelte';
   import DishCard from '../menu/DishCard.svelte';
   import { MenuCategory } from '$lib/common/models/menu';
+  import '../menu/Menu_Kiosko.css'
 
   let menuCategory: MenuCategory[] = [];
 
@@ -16,14 +17,12 @@
   {#each menuCategory as category}
     {#if category.items.length > 0}      
       <section>
-        <details class="rounded-lg overflow-hidden">
+        <details class="border rounded-lg overflow-hidden transition-all duration-300 ease-in-out">
           <summary class="text-2xl font-bold mb-0 cursor-pointer bg-gray-100 p-4 hover:bg-gray-200">
-            <span>{category.name}</span>
-            <svg class="w-[30px] h-[30px] text-gray-800 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 20V7m0 13-4-4m4 4 4-4m4-12v13m0-13 4 4m-4-4-4 4"/>
-            </svg>            
+            <span class='text-gray-600'>{category.name}</span>
+            <span class="view-more">Ver Más</span>
           </summary>
-          <div class="grid grid-cols-1 md:grid-cols-1 gap-6 justify-items-center p-4 bg-white">
+          <div class="grid grid-cols-1 gap-6 justify-items-center p-4 bg-white">
             {#each category.items as item}
               <DishCard {item} />
             {/each}
@@ -33,28 +32,3 @@
     {/if} 
   {/each}
 </div>
-
-<style>
-  summary {
-    list-style: none; /* Quita el triángulo negro */
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  summary::marker {
-    display: none; /* Asegura que el marcador no se muestre */
-  }
-
-  .view-more {
-    font-size: 1rem;
-    color: #3182ce;
-    cursor: pointer;
-    text-decoration: none;
-  }
-
-  summary:hover .view-more {
-    font-size: 1.2rem;
-    transition: ease .5s;
-  }
-</style>
