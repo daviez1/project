2ddd6068a7a71d6e0ef22c0cdc6334ad0fffd763
David Fdez Alonso <fdezd053@gmail.com>
2024-12-25 11:00:05 -0500

    font bold in p

--- src/lib/client/components/home/Hero.svelte
+++ src/lib/client/components/home/Hero.svelte
@@ -16,7 +16,7 @@
   
   <div class="relative z-0 text-center px-4" in:fly>
     <h1 class="text-5xl font-bold mb-4">Bienvenido a nuestro Restaurant</h1>
-    <p class="text-2xl mb-8">Disfruta de la deliciosa comida de esta ciudad</p>
+    <p class="text-2xl font-bold mb-8">Disfruta de la deliciosa comida de esta ciudad</p>
     <div class="space-x-4">
       <a href="/ordenar" class="bg-white text-gray-900 border-2 border-gray px-6 py-3 rounded-lg font-semibold hover:bg-gray/10">
         Ver Menú
